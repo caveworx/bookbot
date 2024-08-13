4f49c06f8f32f4537114a76b7a9f08c3beda88a0
caveworx <dblumley51@gmail.com>
2024-08-12 20:23:16 -0700

    report complete

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_num = get_char_num(text)
-    print(char_num)
-    print(gen_report(char_num))
+    report_data = get_report_data(char_num)
+    gen_report(book_path, num_words, report_data)
 
 
 
+def gen_report(book_path, num_words, report_data):    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
+    for dict in report_data:
+        print(f"The '{dict['name']}' character was found {dict['num']} times")
+    print('--- End report ---')
 
 def get_char_num(text):
     lower_string = text.lower()
@@ -20,9 +26,10 @@ def get_char_num(text):
     return char_count
 
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# need to sort. lisst of dics working
-def gen_report(dict):
+
+def get_report_data(dict):
+    def sort_on(dict):
+        return dict["num"]
     pre_list = []
     for key in dict:
         if key.isalpha() == True:
@@ -30,8 +37,9 @@ def gen_report(dict):
             new_dict['name'] = key
             new_dict['num'] = dict[key]
             pre_list.append(new_dict)
+    pre_list.sort(reverse=True, key=sort_on)
     return pre_list
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 def get_num_words(text):
     words = text.split()
